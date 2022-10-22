6e57fbcc49614abf13a2e42d9155cb30e7416a4a
Ramzan <ihiryded@gmail.com>
2022-10-23 00:29:34 +0300

    На ветке lesson2  Ваша ветка обновлена в соответствии с «origin/lesson2».  Изменения, которые будут включены в коммит: 	изменено:      config/application.rb 	новый файл:    config/version.rb 	новый файл:    public/about.html 	новый файл:    public/about/author.html

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Testguru
+module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.i18n.default_locale = :ru
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.time_zone = "Moscow"
 
     # Configuration for the application, engines, and railties goes here.
     #
