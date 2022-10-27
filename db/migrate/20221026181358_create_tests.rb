6f6be4ee23b534831b79f488670d9464f00c2383
Ramzan <ihiryded@gmail.com>
2022-10-28 00:07:18 +0300

    На ветке lesson3  Изменения, которые будут включены в коммит: исправлен аттрибут correct в миграции create_answers, добавлен  author_id в create_tests

--- db/migrate/20221026181358_create_tests.rb
+++ db/migrate/20221026181358_create_tests.rb
@@ -3,10 +3,18 @@ class CreateTests < ActiveRecord::Migration[7.0]
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, default: '1', null: false
-      t.references :user, null: false, foreign_key: true
+      t.references :author, null: false, foreign_key: { to_table: :users }
       t.references :category, null: false, foreign_key: true
 
       t.timestamps
     end
   end
 end
+
+# При создании теста, про атрибут автор для теста.
+# Лучше если поле в таблице будет называться author_id.
+# Так как по логике нам надо создать связь теста с юзером как автор теста
+# т.е. должно получиться
+# t.references :author...
+# Но учти чтоб внешний ключ смотрел на таблицу юзеров
+# для foreign_key нужно использовать опцию to_table
--- db/migrate/20221026190730_create_answers.rb
+++ db/migrate/20221026190730_create_answers.rb
@@ -2,10 +2,17 @@ class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
       t.text :body, null: false
-      t.integer :status, default: 'correct', null: false
+      t.boolean :correct, default: true, null: false
       t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
   end
 end
+
+# Для атрибута correct у Ответа
+# в миграции нет такого атрибута. 
+# Там почему то стоит статус. Тип integer. А значение по умолчанию текстовое.
+# поле `correct` может по сути иметь только 2 значния "да" или "нет"
+# когда поле подразумевает только 2 значения пользуются типом `boolean`
+# (логическое поле, которое может содержать только `true`/`false` значения)
