b129e1ed306aeaf9d1cec18142b29e53a9e8e4b3
Ramzan <ihiryded@gmail.com>
2022-11-06 15:55:50 +0300

    На ветке lesson4  Изменения, которые будут включены в коммит: 	изменено:      app/models/test.rb 	изменено:      app/models/user.rb 	изменено:      db/migrate/20221101194543_create_tests.rb 	изменено:      db/migrate/20221101194654_create_answers.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,7 @@
 class Test < ApplicationRecord
   def self.by_category(category_title)
     joins('JOIN categories ON categories.id = tests.category_id')
-    .where('categories.title = ?', category_title)
-    .order(title: :desc)
+      .where(categories: {title: category_title})
+      .order(title: :desc)
   end
-
 end
