5feeebb70554cad08a34bc522db356f28539c113
Ramzan <ihiryded@gmail.com>
2022-11-07 09:01:47 +0300

    На ветке lesson4  Изменения, которые будут включены в коммит: 	изменено:      app/models/test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,5 +3,6 @@ class Test < ApplicationRecord
     joins('JOIN categories ON categories.id = tests.category_id')
       .where(categories: {title: category_title})
       .order(title: :desc)
+      .pluck(:title)
   end
 end
