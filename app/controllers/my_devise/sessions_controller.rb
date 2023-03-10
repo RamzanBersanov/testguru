5d8df3997fce5c9feeabfcb2f4910f1b94b30aa5
RamzanBersanov <65899466+RamzanBersanov@users.noreply.github.com>
2023-03-10 22:43:41 +0300

    Update sessions_controller.rb

--- app/controllers/my_devise/sessions_controller.rb
+++ app/controllers/my_devise/sessions_controller.rb
@@ -1,9 +1,6 @@
 class MyDevise::SessionsController < Devise::SessionsController
   def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in, { name: resource.name })
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
+    super
+    flash[:notice] = "Привет, #{resource.name}"
   end
 end 
