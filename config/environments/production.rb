a6ebe54d20a194d900ac53d9c41381b07d8d17ee
Отто Грей <ottogrej@192.168.1.2>
2023-09-02 15:25:40 +0300

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
-    password: 'loam ofrw cuho inmh',
+    password: ENV['SMTP_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true  }
 
