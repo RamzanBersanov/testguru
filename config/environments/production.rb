059f439a16876807c15e045fc0ae872bce065d4c
Отто Грей <ottogrej@192.168.1.2>
2023-09-03 12:30:52 +0300

    env variables not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: 'admfortests@gmail.com',
-    password: 'xtnkgyzdzlmaorct',
+    user_name: 'brrramones@gmail.com',
+    password: 'rgdjkdwivtxcdcxh',
     authentication: 'plain',
     enable_starttls_auto: true  }
 
