9eee42de3742ff4721eae0612b08005a454d48ce
Отто Грей <ottogrej@MacBook-Pro-Otto.local>
2023-09-03 15:50:04 +0300

    var let and const

--- app/javascript/utilities/password_confirmation.js
+++ app/javascript/utilities/password_confirmation.js
@@ -1,17 +1,17 @@
 document.addEventListener('turbolinks:load', function() {
-    let passwords = document.querySelectorAll(".password")
+  let passwords = document.querySelectorAll(".password")
 
-    if(passwords) {
-        passwords.forEach((item) => {
-            item.addEventListener('input', function() {
-                if(passwords[0].value === passwords[1].value) {
-                    document.querySelector('.octicon-passkey-fill').classList.remove('fill_red')
-                    document.querySelector('.octicon-passkey-fill').classList.add('fill_green')
-                } else {
-                    document.querySelector('.octicon-passkey-fill').classList.remove('fill_green')
-                    document.querySelector('.octicon-passkey-fill').classList.add('fill_red')
-                }
-            })
-        })
-    }
+  if(passwords) {
+    passwords.forEach((item) => {
+      item.addEventListener('input', function() {
+      if(passwords[0].value === passwords[1].value) {
+        document.querySelector('.octicon-passkey-fill').classList.remove('fill_red')
+        document.querySelector('.octicon-passkey-fill').classList.add('fill_green')
+      } else {
+        document.querySelector('.octicon-passkey-fill').classList.remove('fill_green')
+        document.querySelector('.octicon-passkey-fill').classList.add('fill_red')
+      }
+      })
+    })
+  }
 })
