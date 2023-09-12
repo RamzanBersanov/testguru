050d309e3fcb1c969ed097f806f7796931bf401a
Отто Грей <ottogrej@192.168.1.4>
2023-09-12 16:01:29 +0300

    исправления 2

--- app/services/assign_badge_service.rb
+++ app/services/assign_badge_service.rb
@@ -9,27 +9,25 @@ class AssignBadgeService
 
   def call
     @badges.each do |badge|
-      if send "#{badge.rule}", badge.given_for
-        @test_passage.user.badges << badge
-      end
+      @test_passage.user.badges << badge if send "#{badge.rule}", badge.given_for
     end
   end
 
   def category?(category)
-    if @test.category.title == category
-      all_tests_in_category = Test.category_title(category).ids
-      @user_successful_tests.where(id: all_tests_in_category).ids.count == all_tests_in_category.count
-    end
+    return unless @test.category.title == category
+
+    all_tests_in_category = Test.category_title(category).ids
+    @user_successful_tests.where(id: all_tests_in_category).ids.count == all_tests_in_category.count
   end
 
   def level?(level)
-    if @test.level == level.to_i 
-      all_tests_in_level = Test.where(level: level.to_i).ids
-      @user_successful_tests.where(id: all_tests_in_level).ids.count == all_tests_in_level.count
-    end 
+    return unless @test.level == level.to_i
+
+    all_tests_in_level = Test.where(level: level.to_i).ids
+    @user_successful_tests.where(id: all_tests_in_level).ids.count == all_tests_in_level.count
   end
 
-  def successful_first_try?(successful_first_try)
+  def successful_first_try?(_successful_first_try)
     @user.test_passages.where(test_id: @test.id).successful.count == 1
-  end 
+  end
 end
--- app/services/create_badge_service.rb
+++ app/services/create_badge_service.rb
@@ -3,35 +3,41 @@ class CreateBadgeService
 
   def initialize(test_passage)
     @test_passage = test_passage
-  end 
+  end
 
   def call
     test = @test_passage.test
-    user = @test_passage.user 
+    user = @test_passage.user
     user.successful_tests << test if @test_passage.successful?
     badge_for_level(user, test)
     badge_for_category(user, test)
-  end 
+  end
 
   def badge_for_level(user, test)
     case test.level
     when 1
       user.badges.build(title: 'easy', picture: 'third.jpg').save if user.successful_tests.easy.distinct == Test.easy
     when 2..4
-      user.badges.build(title: 'intermediate', picture: 'second.jpg').save if user.successful_tests.intermediate.distinct == Test.intermediate
+      if user.successful_tests.intermediate.distinct == Test.intermediate
+        user.badges.build(title: 'intermediate',
+                          picture: 'second.jpg').save
+      end
     when 5..Float::INFINITY
-      user.badges.build(title: 'difficult', picture: 'first.jpg').save if user.successful_tests.difficult.distinct == Test.difficult
-    end 
-  end 
+      if user.successful_tests.difficult.distinct == Test.difficult
+        user.badges.build(title: 'difficult',
+                          picture: 'first.jpg').save
+      end
+    end
+  end
 
   def badge_for_category(user, test)
-    if user.successful_tests.category_title(test.category).distinct == Test.category_title(test.category)
-      case test.category
-      when 'Академические' 
-        user.badges.build(title: test.category.title, picture: 'academic.png').save 
-      when 'Логические'
-        user.badges.build(title: test.category.title, picture: 'logic.png').save 
-      end 
-    end 
-  end 
-end 
+    return unless user.successful_tests.category_title(test.category).distinct == Test.category_title(test.category)
+
+    case test.category
+    when 'Академические'
+      user.badges.build(title: test.category.title, picture: 'academic.png').save
+    when 'Логические'
+      user.badges.build(title: test.category.title, picture: 'logic.png').save
+    end
+  end
+end
